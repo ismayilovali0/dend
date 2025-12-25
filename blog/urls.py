24b0fbde7215e5af8f16714b8b3e3368fd6659df
ismayilovali0 <ismayilovali@protonmail.com>
2025-12-25 14:21:44 +0400

    yenilikler

--- blog/urls.py
+++ blog/urls.py
@@ -6,7 +6,6 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.home_page, name='home_page'), 
     path('blog/', views.blog_page, name='blog_page'),
-    path('aslam/', views.posts_page, name='posts_page'),
     path('posts/', views.post_list, name='post_list'),
     path('post/<int:id>/', views.post, name='post_detail'),
 
